bc418a90fc08b3b01a5337f7ef66db2ba4e9d7c1
dsaunders5358 <dsaunders5358@gmail.com>
2024-04-23 17:36:01 +0100

    Added function count amount of each letters

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = count_words(text)
+    letter_count = count_letter_appearance(text)
     print(f"Found {num_words} in document")
+    print(letter_count)
 
 def count_words(text):
     length_of_text = text.split()
@@ -11,5 +13,17 @@ def count_words(text):
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+def count_letter_appearance(text):
+    letter_count_dict = {}
+    lowered_text = text.lower()
+    for letter in lowered_text:
+        if letter in letter_count_dict:
+            letter_count_dict[letter] += 1
+        elif letter == " ":
+            pass
+        else:
+            letter_count_dict[letter] = 1
+    return letter_count_dict
 
 main()
